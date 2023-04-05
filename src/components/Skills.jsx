68a6127f534246d4bd915dc9fdb443871bf6d384
David Lazaro <dlazaro7@gmail.com>
2023-04-05 19:54:08 +0200

    langs images changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,5 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
 import reactImg from "../assets/images/langs/react.png"
 import bootstrapImg from "../assets/images/langs/bootstrap.png"
 import cssImg from "../assets/images/langs/css.png"
@@ -10,23 +9,64 @@ import jsImg from "../assets/images/langs/js.png"
 // import jwtImg from "../assets/images/langs/jwt.png"
 import mongodbImg from "../assets/images/langs/mongodb.png"
 import nodejsImg from "../assets/images/langs/nodejs.png"
-
 function Skills() {
-  
-
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 6,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div>
-
-      {/* TODO: add animation with my picture */}
       <section className="skill" id="skills">
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <h2>Skills</h2>
               <div className="skill-bx wow zoomIn">
-                
-              
-               
+                <h2>Skills</h2>
+                <Carousel
+                  responsive={responsive}
+                  className="owl-carousel owl-theme skill-slider"
+                  arrows
+                  autoPlay
+                  autoPlaySpeed={2000}
+                  centerMode={false}
+                  containerClass="container-with-dots"
+                  customTransition="all 1s linear"
+                  dotListClass=""
+                  draggable
+                  focusOnSelect={false}
+                  infinite
+                  itemClass=""
+                  keyBoardControl
+                  minimumTouchDrag={80}
+                  pauseOnHover
+                  renderArrowsWhenDisabled={false}
+                  renderButtonGroupOutside={false}
+                  renderDotsOutside={false}
+                  rewind={false}
+                  rewindWithAnimation={false}
+                  rtl={false}
+                  shouldResetAutoplay
+                  showDots={false}
+                  sliderClass=""
+                  slidesToSlide={1}
+                  swipeable
+                  transitionDuration={1000}
+                >
                   <div className="item">
                     <img src={reactImg} alt="react" />
                     <h5>React.js</h5>
@@ -59,7 +99,7 @@ function Skills() {
                     <img src={bootstrapImg} alt="bootstrap" />
                     <h5>Bootstrap</h5>
                   </div>
-               
+                </Carousel>
               </div>
             </div>
           </div>
@@ -68,5 +108,4 @@ function Skills() {
     </div>
   );
 }
-
 export default Skills;
