634f0e57bc68be4d1e4ccc9cc02b65d1af8db193
David Lazaro <dlazaro7@gmail.com>
2023-04-18 01:46:41 +0200

    credentials main comp erased

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import Banner from "./components/Banner";
-import Credentials from "./components/Credentials";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Credentials />
       <Projects />
       <Footer />
     </div>
