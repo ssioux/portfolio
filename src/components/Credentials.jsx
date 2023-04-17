01c55a0d4ea596a55f707f67f2624ce5cfc1de41
David Lazaro <dlazaro7@gmail.com>
2023-04-17 19:22:33 +0200

    credentials comp html added

--- src/components/Credentials.jsx
+++ src/components/Credentials.jsx
@@ -1,7 +1,21 @@
+// Credentials
+import ironHack from "../assets/credentials/IronHack_dev_certificate.png";
+import gitHub from "../assets/credentials/Git-Certificate.jpg";
+
 function Credentials() {
   return (
-    <div>Credentials</div>
-  )
+    <section className="credentials-main">
+      <h2>Credentials</h2>
+      <div className="crededentials-container">
+        <div className="credentials-pic">
+          <img src={ironHack} alt="ironHack_certificate" />
+        </div>
+        <div className="credentials-pic">
+          <img src={gitHub} alt="gitHub_certificate" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Credentials
+export default Credentials;
